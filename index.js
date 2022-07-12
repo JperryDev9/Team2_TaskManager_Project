380de09d96f38529590d4ed6a04b272e282b8982
Tori <105828543+heyimvictoria@users.noreply.github.com>
2022-07-12 14:59:49 -0500

    created index.js file
    created index.js file for task 6, created validFormInput function

---
+++ index.js
@@ -0,0 +1,25 @@
+function validFormFieldInput(data) {
+  const company = document.querySelector("#company");
+    const name = company.value;
+  const recruiter = document.querySelector("#recruiter");
+    const name = recruiter.value;
+  const description = document.querySelector("#description");
+    const name = description.value;
+  const start = document.querySelector("#start");
+    const name = start.value;
+  const newJobButton = document.querySelector("#newJobButton");
+    const name = newJobButton.value;
+  console.log("name:  "+company);
+  console.log("name:  "+recruiter);
+  console.log("name:  "+description);
+  console.log("name:  "+start);
+  console.log("name:  "+newJobButton);
+  
+  /*const "" = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} */
+
