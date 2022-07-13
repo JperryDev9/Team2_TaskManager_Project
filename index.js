1c0f86fba484426fa9e8c0f32bbb355308c5dfb5
Tori <105828543+heyimvictoria@users.noreply.github.com>
2022-07-13 10:20:32 -0500

    update index.js
    updated index.js, fix errors and removed console.log,  added eventListener and render

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
+const newTaskForm = document.querySelector('#newTaskForm');
+
+newTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
 function validFormFieldInput(data) {
   const name = document.querySelector("#name").value;
-
   const assignedTo = document.querySelector("#assignedTo").value;
-
   const description = document.querySelector("#description").value;
-
-  const dueDue = document.querySelector("#dueDate").value;
- 
+  const dueDate = document.querySelector("#dueDate").value;
   const status = document.querySelector("#status").value;
   
-  console.log("name:  "+name);
+  TaskManager.addTask(name, description, assignedTo, dueDate, status);
+  
+  /*console.log("name:  "+name);
   console.log("name:  "+assignedTo);
   console.log("name:  "+description);
   console.log("name:  "+dueDate);
-  console.log("name:  "+status);
+  console.log("name:  "+status);*/
   
-  const "" = document.getElementById("myDIV");
+
+    TaskManager.render();
+
+    name.value = '';
+    description.value = '';
+    assignedTo.value = '';
+    dueDate.value = '';
+    status.value = '';
+  
+  /*const "" = document.getElementById("myDIV");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
-  }
-}
-
+    }
+  }*/
+  
+});
