d82ea4db393a528a16a9b30e5d08c182978e532d
Jessica Perry <l33thedgieperry@gmail.com>
2022-07-13 15:48:54 -0700

    no major changes, just testing how to comment

--- Javascript/taskManager.js
+++ Javascript/taskManager.js
@@ -1,16 +1,16 @@
 document.getElementById("myBtn").addEventListener("click", displayDate);
 
-function createTaskHtml (name, description, assignedTo, dueDate, status) {
-    const task = {
-        // Increment the currentId property
-        id: this.currentId++,
-        name: name,
-        description: description,
-        assignedTo: assignedTo,
-        dueDate: dueDate,
-        status: 'TODO'
-    }
-        const html = ` 
+function createTaskHtml(name, description, assignedTo, dueDate, status) {
+  const task = {
+    // Increment the currentId property
+    id: this.currentId++,
+    name: name,
+    description: description,
+    assignedTo: assignedTo,
+    dueDate: dueDate,
+    status: "TODO",
+  };
+  const html = ` 
         <div class="card" style="width: 18rem";>
            <div class="card-body">
             <h5 class="card-title">${name}</h5>
@@ -33,57 +33,52 @@ function createTaskHtml (name, description, assignedTo, dueDate, status) {
             <a href="#" class="btn btn-success">Button</a>
           </div>
         </div>
-        `
-     }; 
-    
-
-
-
-
-
+        `;
+}
 
 class taskManager {
   constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
-        
-        console.log(taskManager);
+    this.tasks = [];
+    this.currentId = currentId;
+
+    console.log(taskManager);
+  }
+  addTask(name, description, assignedTo, dueDate) {
+    const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: "TODO",
+    };
+    this.tasks.push(task);
+    console.log(taskManager);
+  }
+
+  render() {
+    const tasksHtmlList = [];
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+      const date = new Date(task.dueDate);
+      const formattedDate =
+        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+      const taskHtml = createTaskHtml(
+        task.name,
+        task.description,
+        task.assignedTo,
+        formattedDate,
+        task.status
+      );
+      tasksHtmlList.push(taskHtml);
     }
-    addTask(name, description, assignedTo, dueDate) {
-        const task = {
-            id: this.currentId++,
-            name: name,
-            description: description,
-            assignedTo: assignedTo,
-            dueDate: dueDate,
-            status: 'TODO'
-        };
-       this.tasks.push(task);
-       console.log(taskManager);
-    }
- 
-    render() {
-      const tasksHtmlList = []
-           for (let i = 0; i < this.tasks.length; i++) {
-            const task = this.tasks[i]
-            const date = new Date(task.dueDate);
-            const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
-            const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status);
-            tasksHtmlList.push(taskHtml)
-           }
 
-           const tasksHtml = tasksHtmlList.join('\n');
+    const tasksHtml = tasksHtmlList.join("\n");
 
-           const tasksList = document.querySelector('#tasksList');
-           tasksList.innerHTML = tasksHtml;
-        }
+    const tasksList = document.querySelector("#tasksList");
+    tasksList.innerHTML = tasksHtml;
+  }
+}
 
-        
-};
-
-
-
-   
-    
-    newTaskVar = new taskManager();
-    newTaskVar.addTask();
+newTaskVar = new taskManager();
+newTaskVar.addTask();
