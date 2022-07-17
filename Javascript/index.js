135100bbe40e212484ed8aa67fe9169f7c0d91aa
Ado Apenteng <66426822+adocodes@users.noreply.github.com>
2022-07-17 19:28:20 -0400

    Update index.js
    added a querySelector & a  event listener to select the Task List.

--- Javascript/index.js
+++ Javascript/index.js
@@ -6,3 +6,18 @@ taskHtml = createTaskHtml(
   "TODO"
 );
 console.log(taskHtml);
+
+
+
+const tasksList = document.querySelector('#tasksList');
+
+tasksList.addEventListener('click', (event) => {
+
+    if (event.target.classList.contains('done-button')) {
+     const parentTask = event.target.parentElement.parentElement;
+       const taskId = Number(parentTask.dataset.taskId);
+        const task = taskManager.getTaskById(taskId);
+          task.status = 'DONE';
+           taskManager.render();
+    }
+});
